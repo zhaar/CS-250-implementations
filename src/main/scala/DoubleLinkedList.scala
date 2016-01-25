eda6f4cb38d6e258cb8bdcc79c7f990d21fdc18e
zephyz <zephyz.zhaar@gmail.com>
2016-01-25 02:32:18 +0100

    implement stateful double linked lists

--- src/main/scala/DoubleLinkedList.scala
+++ src/main/scala/DoubleLinkedList.scala
@@ -1,43 +1,91 @@
 
 /*
-* A double linked list is a list with each node having a pointer to it's next and previous neighbor
-* algebraically we could define it like so: L = 1 + (L * T * L)
-* so it's either an empty list or a triple of a List, an element and another List
-* The problem with this implementation is that we don't make use of persistent data and
-* our insert function will then run in O(n) which we do not want.
-* A better solution would be to use some sort of zipper with would look like this:
-* 1 +
-*/
+ * double linked lists are impossible in a stateless setting, but zippers on lists
+ * are close enough
+ */
+case class Zipper[T](left: List[T], e: T, right: List[T]) extends DoubleLinkedList[T] {
+  def isEmpty = false
+  def next = Zipper(left.tail, left.head, e :: right)
+  def prev = Zipper(e :: left, right.head, right.tail)
+  def head = e
+  def insert[S >: T](t:S) = insertBefore(t)
+  def insertAfter[S >: T](t: S): DoubleLinkedList[S] = Zipper(e :: left, t,right)
+  def insertBefore[S >: T](t: S): DoubleLinkedList[S] = Zipper(left, t, e :: right)
+}
+
 trait DoubleLinkedList[+T] {
   def isEmpty: Boolean
-  def next: DoubleLinkedList[T]
-  def prev: DoubleLinkedList[T]
-  def elem: T
-  def insertAfter[S >: T](t: S): DoubleLinkedList[S]
-  def insertBefore[S >: T](t: S): DoubleLinkedList[S]
+  def head: T
+  def insert[S >: T](t: S): DoubleLinkedList[S]
+}
+//had to ditch variance to comply with insert and remove
+trait MutableDLL[T] {
+  def isEmpty: Boolean
+  def head: Option[T]
+  def insert(t: T): Unit
+  def remove(e: T): Unit
 }
 
-case object Empty extends DoubleLinkedList[Nothing] {
-  def isEmpty = true
-  def next = throw new NoSuchElementException("Empty has no next element")
-  def prev = throw new NoSuchElementException("Empty has no previous element")
-  def elem = throw new NoSuchElementException("Empty has no value")
-  def insertAfter[T](t: T) = Node(Nil, t, Nil)
-  def insertBefore[T](t: T) = Node(Nil, t, Nil)
-}
+//The actual mutable linked list took much more work than the stateless one
+class MutableLinkedList[T] extends MutableDLL[T] {
 
-case class Node[T](left: List[T], e: T, right: List[T]) extends DoubleLinkedList[T] {
-  def isEmpty = false
-  def next = Node(left.tail, left.head, e :: right)
-  def prev = Node(e :: left, right.head, right.tail)
-  def elem = e
-  def insertAfter[S >: T](t: S): DoubleLinkedList[S] = Node(e :: left, t,right)
-  def insertBefore[S >: T](t: S): DoubleLinkedList[S] = Node(left, t, e :: right)
-}
+  sealed trait LLNode
 
-object DoubleLinkedList {
-  def map[T, S](list: DoubleLinkedList[T], f: T => S): DoubleLinkedList[S] = list match {
-    case Empty => Empty
-    case Node(p, e, n) => Node(p map f, f(e), n map f)
+  case class LLSentinel(var pointer: LLNode) extends LLNode
+
+  case class LLElem(var prev: LLNode, elem: T, var next: LLNode) extends LLNode
+
+  val start = LLSentinel(null)
+  val end = LLSentinel(start)
+  start.pointer = end
+
+  override def head: Option[T] = start.pointer match {
+    case s: LLSentinel => None
+    case LLElem(_, e, _) => Some(e)
+  }
+
+  def isEmpty = start.pointer == end
+
+  def insert(e: T): Unit = {
+    val n = LLElem(null, e, null)
+    val nxt = start.pointer
+    val lst = end.pointer
+    n.prev = lst
+    n.next = nxt
+    nxt match {
+      case s: LLSentinel => s.pointer = n
+      case elem: LLElem => elem.prev = n
+    }
+    lst match {
+      case s: LLSentinel => s.pointer = n
+      case elem: LLElem => elem.next = n
+    }
+    this
+  }
+
+  def remove(e: T): Unit = {
+    this.find(e).foreach( (elem: LLElem) => {
+      val previous = elem.prev
+      val next = elem.next
+      previous match {
+        case s: LLSentinel => s.pointer = elem.next
+        case e: LLElem => e.next = elem.next
+      }
+      next match {
+        case s: LLSentinel => s.pointer = elem.prev
+        case e: LLElem => e.prev = elem.prev
+      }
+
+    }
+    )
+
+  }
+  def find(t: T): Option[LLElem] = {
+
+    def loop(node: LLNode): Option[LLElem] = node match {
+      case elem @ LLElem(_, e, n) => if (e == t) Some(elem) else loop(n)
+      case _ => None
+    }
+    loop(start.pointer)
   }
 }
