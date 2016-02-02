037546528223c2ba28d67026e8803abfbe4c56eb
zephyz <zephyz.zhaar@gmail.com>
2016-02-02 18:05:17 +0100

    fix project migration artifact

--- src/main/scala/DisjointSets.scala
+++
@@ -1,8 +0,0 @@
-trait ArbitrarySet[T] {
-  def add(elem: T): ArbitrarySet[T]
-}
-
-case class DisjointSet[T](representative: T, associated: List[T]) extends ArbitrarySet[T] {
-  def add(elem: T) = this.copy(associated = elem :: associated)
-  def find(elem: T) = associated.find(elem)
-}
